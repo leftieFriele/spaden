8df991449df900a49376ddd18817b3c8114a24a9
davhaset <david.hasaether@finn.no>
2015-01-29 13:03:51 +0100

    Remove sourcemaps support

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,17 @@
-var concat = require('gulp-concat');
-var filesize = require('gulp-filesize');
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 var gzip = require('gulp-gzip');
 var minifyCSS = require('gulp-minify-css');
-var pkg = require('./package');
 var rename = require('gulp-rename');
 var replace = require('gulp-replace');
 var rimraf = require('gulp-rimraf');
-var sourcemaps = require('gulp-sourcemaps');
-var spawn = require('child_process').spawn;
 var tar = require('gulp-tar');
 var util = require('gulp-util');
+var pkg = require('./package');
 
 var distDir = './dist/';
 var buildDir = './src/styles/';
 var destinationDir = distDir + pkg.name + '-' + pkg.version;
-
-var debugMode = false;
 
 gulp.task('clean', function() {
     return gulp.src([
@@ -48,23 +43,19 @@ gulp.task('build-so-css', ['sass'], function() {
             buildDir + '/core/core.css',
             buildDir + '/components/components.css'
         ])
-        .pipe(sourcemaps.init())
         .pipe(minifyCSS({
             keepBreaks: true,
             processImport: true,
             debug: false
         }))
         .pipe(concat('spaden.css'))
-        .pipe(sourcemaps.write())
         .pipe(gulp.dest(destinationDir + '/styles/'))
-        .pipe(sourcemaps.init())
         .pipe(minifyCSS({
             keepBreaks: false,
             processImport: true,
             debug: true
         }))
         .pipe(concat('spaden.min.css'))
-        .pipe(sourcemaps.write())
         .pipe(gulp.dest(destinationDir + '/styles/'))
 });
 
@@ -84,14 +75,12 @@ gulp.task('build-legacy-css', ['sass'], function() {
     gulp.src(buildDir + '/legacy/**')
         .pipe(concat('legacy.css'))
         .pipe(gulp.dest(destinationDir + '/styles/'))
-        .pipe(sourcemaps.init())
         .pipe(minifyCSS({
             keepBreaks: false,
             processImport: true,
             debug: false
         }))
         .pipe(concat('legacy.min.css'))
-        .pipe(sourcemaps.write())
         .pipe(gulp.dest(destinationDir + '/styles/'))
 });
 
