e61e086c21c0ae2e1d4aa8d81a184e5117dba52b
davhaset <david.hasaether@finn.no>
2015-02-03 14:44:49 +0100

    Fix a path in server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require("express");
+var path = require("path");
 
 var app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "src")));
 app.get("/", function(req, res) {
   res.set("Content-Type", "text/css");
-  res.send("@import '/src/styles/core/core.css';\n" +
-           "@import '/src/styles/components/components.css';");
+  res.send("@import '/styles/core/core.css';\n" +
+           "@import '/styles/components/components.css';");
 });
 app.listen(process.env.PORT || 3000);
